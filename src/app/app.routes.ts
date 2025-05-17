a0d2b392fb0c51b1b93e905611629fc29bfa5291
saharm5 <saharmohammadi0224@gmail.com>
2025-05-17 09:46:59 +0330

    updating

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,12 @@
-// /* eslint-disable id-length */
-// import { Routes } from '@angular/router';
-// import { HomeComponent } from './pages/home/home.component';
-// import { FundDetailComponent } from './pages/fund-detail/fund-detail.component';
-// // import { TreeMapPageComponent } from './pages/tree-map-page/tree-map-page.component';
-// // import { NotFoundComponent } from './features/not-found/not-found.component';
 
-// export const routes: Routes = [
-//   { path: '', component: HomeComponent },
-//   { path: 'fund/:id', component: FundDetailComponent },
-//   {
-//     path: 'treemap',
-//     loadChildren: () => import('./pages/tree-map-page/tree-map.routes').then(m => m.routes)
-//   }
-  
-//   // { path: '**', component: NotFoundComponent }
-// ];
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { TreeMapComponent } from './core/shared/tree-map/tree-map.component';
+import { FundDetailComponent } from './pages/fund-detail/fund-detail.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'treemap', component: TreeMapComponent },
+  { path: 'fund/:id', component: FundDetailComponent },
+
 ];
